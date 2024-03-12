14257ea283b029fcbfacb01ce30eaf0190ddf30a
Leandro <alianoleandro0610@outlook.com>
2024-03-12 14:00:31 -0300

    initial configuration

---
+++ src/main/java/com/reservahotel/ReservahotelApplication.java
@@ -0,0 +1,13 @@
+package com.reservahotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservahotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservahotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reservahotel/ReservahotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservahotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservahotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
