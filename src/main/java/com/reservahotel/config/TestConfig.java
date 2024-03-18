b5af25ddc1b28d3c7c7fe1546f3b2efef536ac2b
Leandro <alianoleandro0610@outlook.com>
2024-03-18 17:44:13 -0300

    configuration

--- src/main/java/com/reservahotel/config/TestConfig.java
+++ src/main/java/com/reservahotel/config/TestConfig.java
@@ -1,21 +1,33 @@
 package com.reservahotel.config;
 
+import com.reservahotel.domain.entities.Reserve;
 import com.reservahotel.domain.entities.User;
+import com.reservahotel.repositories.ReserveRepository;
 import com.reservahotel.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ReserveRepository reserveRepository;
     @Override
     public void run(String... args) throws Exception {
 
         User u1 = new User("leandro","aliano","1234","leandro.email.com","1234");
         userRepository.save(u1);
+        Reserve r1 = new Reserve(LocalDate.now(),u1);
+        Reserve r2 = new Reserve(LocalDate.now(),u1);
+        Reserve r3 = new Reserve(LocalDate.now(),u1);
+        reserveRepository.saveAll(Arrays.asList(r1,r2,r3));
     }
 }
